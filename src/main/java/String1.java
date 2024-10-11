186e57448008eb77fa9ac19dea49495931559926
SaiD867 <saidoddapaneni18@gmail.com>
2024-10-11 09:19:37 -0700

    Update String1.java

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -58,16 +58,10 @@ public class helloName {
      * makeAbba("Yo", "Alice") → "YoAliceAliceYo"
      * makeAbba("What", "Up") → "WhatUpUpWhat"
      */
-public class abba {
     public String makeAbba(String a, String b) {
         return a+b+b+a;
     }
-        public static void main(String[] args) {
-            abba obj = new abba();
-        String greeting = obj.makeAbba("Bob", "hi"); 
-        System.out.println(greeting); // Output: Hello Bob!
-    }
-}
+
 
     /*
      * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. 
@@ -77,16 +71,11 @@ public class abba {
      * makeTags("i", "Hello") → "<i>Hello</i>"
      * makeTags("cite", "Yay") → "<cite>Yay</cite>"
      */
-public class tags {
+
     public String makeTags(String tags,String word){
     return "<"+tags+">"+word+"</"+tags+">";
     }
-    public static void main(String[] args)
-    {
-        tags obj = new tags();
-        String hi = obj.makeTags("cite", "Yay");
-        System.out.println(hi);
-    }}
+
     
 
     /*
@@ -99,15 +88,8 @@ public class tags {
      * makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
      * makeOutWord("[[]]", "word") → "[[word]]"
      */
- public class OutWord {
     public String makeOutWord(String out, String word) {
         return out.substring(0, 2) + word +out.substring(2);}
-    public static void main(String[] args)
-    {
-        OutWord obj = new OutWord();
-        String hi = obj.makeOutWord("[[]]", "word");
-        System.out.println(hi);
-    }}
 
     /*
      * Given a string, return a new string made of 3 copies of the last 2 chars of the original string. 
@@ -116,18 +98,12 @@ public class tags {
      * extraEnd("ab") → "ababab"
      * extraEnd("Hi") → "HiHiHi"
      */
-    public class End1 {
+  
         public String extraEnd3(String word) {
             String lastChars = word.substring(word.length() - 2);
             return lastChars + lastChars + lastChars;
         }
-        public static void main(String[] args)
-        {
-            End1 obj = new End1();
-            String hi = obj.extraEnd3("ab");
-            System.out.println(hi);
-        }}
-
+       
     /*
      * Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
      * If the string is shorter than length 2, return whatever there is, 
@@ -137,19 +113,14 @@ public class tags {
      * firstTwo("abcdefg") → "ab"   
      * firstTwo("ab") → "ab"
      */
-    public class End2 {
+
         public String extraEnd2(String word) {
             if (word.length()>3)
             {return word.substring(0,2); }
             else
             {return word.substring(0,2) ;}
         }
-        public static void main(String[] args)
-        {
-            End2 obj = new End2();
-            String hi = obj.extraEnd2("ab");
-            System.out.println(hi);
-        }}
+
 
     /*
      * Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".]
@@ -157,16 +128,10 @@ public class tags {
      * firstHalf("HelloThere") → "Hello"
      * firstHalf("abcdef") → "abc"
      */
-    public class End1 {
         public String firstHalf(String str) {
             return str.substring(0, str.length() / 2);
         }
-    
-        public static void main(String[] args) {
-            End1 obj = new End1();
-            System.out.println(obj.firstHalf("WooHoo")); 
-        }
-    }
+
     
 
     /*
@@ -176,18 +141,11 @@ public class tags {
      * withoutEnd("java") → "av"
      * withoutEnd("coding") → "odin"
      */
-    public class End3 {
         public String withoutEnd(String word) {
 
  return  word.substring(1,word.length()-1);
         }
-        public static void main(String[] args)
-        {
-            End3 obj = new End3();
-            String hi = obj.withoutEnd("abyuyyu");
-            System.out.println(hi);
-        }}
-
+       
     /*
      * Given 2 strings, a and b, return a string of the form short+long+short, with the 
      * shorter string on the outside and the longer string on the inside. 
@@ -197,19 +155,13 @@ public class tags {
      * comboString("aaa", "b") → "baaab"
      */
 
-     public class End4 {
+
         public String comboString(String a, String b) {
         if(a.length()>b.length())
-{return b+a+b;}
-else{return a+b+a ;}
+            {return b+a+b;}
+        else
+            {return a+b+a ;}
         }
-        public static void main(String[] args)
-        {
-            End4 obj = new End4();
-            String hi = obj.comboString("abyuyyu","uiu");
-            System.out.println(hi);
-        }
-    }
 
     /*
      * Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". 
@@ -219,15 +171,11 @@ else{return a+b+a ;}
      * middleThree("solving") → "lvi"
      */
     
-    public class End5 {
+
         public String middleThree(String str) {
             int midIndex = str.length() / 2;
             return str.substring(midIndex - 1, midIndex + 2);
-        }
-        public static void main(String[] args) {
-            End5 obj = new End5();
-            System.out.println(obj.middleThree("Candy"));
-        }
+    
     }
     /*
      * Given a string, return a new string made of 3 copies of the first 2 chars of the original string. 
@@ -236,19 +184,14 @@ else{return a+b+a ;}
      * extraFront("ab") → "ababab"
      * extraFront("H") → "HHH"
      */
-    public class End6{
+
         public String extraFront(String str) {
             if (str.length()> 2)
             {return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);}
     else{return str.substring(0, 1) + str.substring(0, 1) + str.substring(0, 1);}
     
         }
-    
-        public static void main(String[] args) {
-            End6 obj = new End6();
-            System.out.println(obj.extraFront("He"));
-        }
-    }
+
 
     /*
      * Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. 
@@ -257,19 +200,12 @@ else{return a+b+a ;}
      * left2("java") → "vaja"
      * left2("Hi") → "Hi"
      */
-    public class End7{
+
     public String left2(String str) {
         // Move the first 2 characters to the end
         return str.substring(2) + str.substring(0, 2);
     }
 
-    public static void main(String[] args) {
-        End7 obj = new End7();
-        System.out.println(obj.left2("Hello")); 
-        System.out.println(obj.left2("java"));  
-        System.out.println(obj.left2("Hi"));   
-    }
-}
 
     /*
      * Given a string, return true if "bad" appears starting at index 0 or 1 in the string, 
@@ -280,7 +216,6 @@ else{return a+b+a ;}
      * hasBad("xxbadxx") → false
      */
 
-     public class End12{
         public boolean hasBad(String str) {
             int Index = str.indexOf("bad");
             if (Index == 0)
@@ -288,13 +223,6 @@ else{return a+b+a ;}
             else{return true;}
             ;
         }
-        public static void main(String[] args) {
-            End12 obj = new End12
-            System.out.println(obj.hasBad("Hello"));
-            System.out.println(obj.hasBad("badxx"));
-            System.out.println(obj.hasBad("xbadxx"));
-    }}
-
 
 
 
@@ -306,24 +234,17 @@ else{return a+b+a ;}
      * conCat("dog", "cat") → "dogcat"
      * conCat("abc", "") → "abc"
      */
-    public class End8{
+
         public String conCat(String a, String b) {
             // Check if the last character of 'a' is the same as the first character of 'b'
             if (a.substring(1).equals(b.subtring(0,1))) {
                 // Concatenate 'a' and 'b', omitting the first character of 'b'
                 return a + b.substring(b.length() -1);
             }
-            // Otherwise, just concatenate 'a' and 'b'
+            // Otherwise, just concatenate 'a' and 'b' con - cat - ate = with cat eat 
             return a + b;
         }
-    
-        public static void main(String[] args) {
-            End8 obj = new End8();
-            System.out.println(obj.conCat("abc", "cat")); // Should print "abcat"
-            System.out.println(obj.conCat("dog", "cat")); // Should print "dogcat"
-            System.out.println(obj.conCat("abc", ""));    // Should print "abc"
-        }
-    }
+   
 
     /*
      *Given two strings, append them together (known as "concatenation") and return the result. 
@@ -336,20 +257,12 @@ else{return a+b+a ;}
      *minCat("java", "Hello") → "javaello"
      */
 
-     public class End9 {
         public String minCat(String a, String b) {
         if(a.length()>b.length())
 { String total = a.substring(a.length()-b.length()) + b;
     return total;}
 else{String total = a+b.substring(b.length() - a.length());
     return total ;}
-        }
-        public static void main(String[] args)
-        {
-            End9 obj = new End9();
-            String hi = obj.minCat("Hel","Hello");
-            System.out.println(hi);
-        }
     }
 
     /*
@@ -359,7 +272,7 @@ else{String total = a+b.substring(b.length() - a.length());
      * withoutX("xHi") → "Hi"
      * withoutX("Hxix") → "Hxi"
      */
-    public class End10 {
+  
     public String withoutX(String str) {
         if (str.length() == 0) {
             return str;
@@ -373,13 +286,6 @@ m         if (str.length() > 0 && str.charAt(str.length() - 1) == 'x') {
         
         return str;
     }
-    public static void main(String[] args)
-        {
-            End10 obj = new End10();
-            String hi = obj.withoutX("Hel","Hello");
-            System.out.println(hi);
-        }
-    }
     /*
      * Given a string, return a version without the first 2 chars. 
      * Except keep the first char if it is 'a' and keep the second char if it is 'b'. 
@@ -389,7 +295,6 @@ m         if (str.length() > 0 && str.charAt(str.length() - 1) == 'x') {
      * deFront("java") → "va"
      * deFront("away") → "aay"
      */
-    public class End11 {
         public String deFront(String str) {    
          if (str.length()<2)   
         return str;
@@ -404,10 +309,4 @@ m         if (str.length() > 0 && str.charAt(str.length() - 1) == 'x') {
     
         return result;
     }}
-    public static void main(String[] args)
-            {
-                End11 obj = new End11();
-                String hi = obj.deFront("ayakglkduii");
-                System.out.println(hi);
-            }
-        }
+
